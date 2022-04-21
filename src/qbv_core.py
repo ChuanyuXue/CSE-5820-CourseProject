a0df80fbc1d2328908de6f9737992a8749be0137
ChuanyuXue <chuanyu.xue@uconn.edu>
2022-04-21 19:17:58 -0400

    update

--- src/qbv_core.py
+++ src/qbv_core.py
@@ -1,7 +1,11 @@
+from distutils.log import error
 from time import time
 from typing import *
 
 from src.utils import time_duration, time_point, clock
+
+_devices = []
+_flows = []
 
 
 class link:
@@ -10,23 +14,31 @@ class link:
         self.ingress_id = x
         self.egress_id = y
 
+    def __getitem__(self, x: int) -> int:
+        if x == 0:
+            return self.ingress_id
+        elif x == 1:
+            return self.egress_id
+        else:
+            return error("Index out of range")
+
 
 class frame:
 
-    def __init__(self, id: int, p: time_duration, l: int,
-                 o: time_point) -> None:
+    def __init__(self, id: int, flow_id: int, o: time_point) -> None:
         self.id = id
-        self.period = p
-        self.length = l
         self.release_t = o
+        self.flow_id = flow_id
 
 
 class flow:
 
-    def __init__(self, id: int, pcp: int, frames: Tuple[frame],
-                 route: Tuple[link]) -> None:
+    def __init__(self, id: int, p: time_duration, l: int, pcp: int,
+                 frames: Tuple[frame], route: Tuple[link]) -> None:
         self.id = id
         self.pcp = pcp
+        self.period = p
+        self.length = l
 
         self.frames = frames
         self.route = route
@@ -47,8 +59,6 @@ class queue:
     def get(self, ) -> frame:
         return self._data.pop(0)
 
-    
-
 
 class gcl:
 
@@ -60,13 +70,12 @@ class gcl:
         self.length = len(self.gate_time)
 
         self._max_time = t(0, 0) + self.period
-        
+
         ## Close time of gate i at t
         self._close_time_map = {}
         for i, t in range(self.gate_time):
             for k, v in self.gate_event[i]:
                 pass
-
 
     def get_current_status(self,
                            t: time_point) -> Tuple[List[bool], time_duration]:
@@ -105,7 +114,7 @@ class egress:
                  prio_to_tc: Dict[int:int]) -> None:
         '''
 
-        speed: bytes per-second
+        speed: bytes per-nanosecond
         neighbor: ID of adjancent device
         queues: ordered list of egress queues
 
@@ -119,6 +128,13 @@ class egress:
         self.neighbor = neighbor
         self.pcp_to_prio = pcp_to_prio
         self.prio_to_tc = prio_to_tc
+        self.tc_to_prio = {}
+        for key, value in self.tc_to_prio.items():
+            if value in self.tc_to_prio:
+                print(
+                    "[!] Warning: Priority to Traffic class is not one-to-one mapping!"
+                )
+            self.tc_to_prio[value] = key
 
     def check_aviable(self, ) -> int:
         '''
@@ -130,22 +146,36 @@ class egress:
         ## if current port is not aviable (transmitting data)
         if not self.clock.is_aviable:
             return -1
-        status, left_time = self.gcl.get_current_status(self.clock.current_time)
+        status, left_time = self.gcl.get_current_status(
+            self.clock.current_time)
 
         ## Check if the left time is still enough for frame
         for i, v in status:
             if v and left_time < self.queues[i][0].length / self.speed:
                 status[i] = False
-        
-        
+
+        ## Compete by priority
+        _max_prio = -1
+        _max_index = None
+        for i, v in status:
+            if self.tc_to_prio[i] > _max_prio:
+                _max_index = i
+                _max_prio = self.tc_to_prio
+
+        if _max_index >= 0:
+            self.transmit(_max_index)
 
     def transmit(self, i: int):
-        pass
+        frame = self.queues[i].get()
+        for a, b in _flows[frame.flow_id].route:
+            if self.id == a:
+                _devices[b].recv(frame)
 
 
 # class ingress:
 #     '''
 #     No ingress concept for now.
+#     Currently replaced by device.recv function
 #     '''
 
 #     def __init__(self, ) -> None:
@@ -154,5 +184,9 @@ class egress:
 
 class device:
 
-    def __init__(self, id: int) -> None:
+    def __init__(self, id: int, egress_ports: List[egress]) -> None:
         self.id = id
+        self.egress_ports = egress_ports
+
+    def recv(self, frame: frame) -> None:
+        pass
